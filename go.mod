0b9be0a4ae7c25a287a3066e04dc219b75254936
Andrey Petrov <andrey.petrov@shazow.net>
2020-06-11 12:21:38 -0400

    go mod init; go get -u

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gitlab.com/shazow/news.shazow.net
+
+go 1.14
+
+require (
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
+	github.com/go-chi/chi v4.1.2+incompatible // indirect
+	github.com/pressly/chi v4.1.2+incompatible
+)
--- main.go
+++ main.go
@@ -53,7 +53,6 @@ func main() {
 	r.Use(middleware.RealIP)
 	//r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(middleware.CloseNotify)
 	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
