b2089044fb8fd4e614293148e7831a829f87ba56
Andrey Petrov <andrey.petrov@shazow.net>
2020-08-18 17:05:51 -0400

    internal/httphelper: Add TransportWithAgent

--- internal/httphelper/httphelper.go
+++ internal/httphelper/httphelper.go
@@ -15,3 +15,17 @@ func (rt FixedRoundTrip) RoundTrip(_ *http.Request) (*http.Response, error) {
 		Header:     make(http.Header),
 	}, nil
 }
+
+// TransportWithAgent is an http.RoundTripper transport wrapper
+// that adds default headers and configurations.
+type TransportWithAgent struct {
+	http.RoundTripper
+	UserAgent string
+}
+
+func (transport TransportWithAgent) RoundTrip(req *http.Request) (*http.Response, error) {
+	if transport.UserAgent != "" {
+		req.Header.Add("User-Agent", transport.UserAgent)
+	}
+	return transport.RoundTripper.RoundTrip(req)
+}
