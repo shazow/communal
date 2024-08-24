d23989c3da4afda68d480a827fc09d4dd4243aeb
Andrey Petrov <andrey.petrov@shazow.net>
2024-08-24 12:04:54 -0400

    internal: Add http transport logging

--- internal/httphelper/httphelper.go
+++ internal/httphelper/httphelper.go
@@ -4,6 +4,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+
+	"github.com/rs/zerolog"
 )
 
 type FixedRoundTrip string
@@ -21,11 +23,15 @@ func (rt FixedRoundTrip) RoundTrip(_ *http.Request) (*http.Response, error) {
 type TransportWithAgent struct {
 	http.RoundTripper
 	UserAgent string
+	Logger    zerolog.Logger
 }
 
 func (transport TransportWithAgent) RoundTrip(req *http.Request) (*http.Response, error) {
 	if transport.UserAgent != "" {
 		req.Header.Add("User-Agent", transport.UserAgent)
 	}
+
+	transport.Logger.Debug().Str("method", req.Method).Str("url", req.URL.String())
+
 	return transport.RoundTripper.RoundTrip(req)
 }
